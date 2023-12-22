abcfdc16b9be07e8a50e4b2cd0a0c8ffe1db76bd
Jordyrepi <148455769+Jordyrepi@users.noreply.github.com>
2023-12-22 13:54:03 +0700

    feat:add feature (#1)

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import userRouters from "./routers/userRouters";
+
+// port untuk localhost:.....
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+//middleware
+app.use(express.json());
+
+app.use("/users", userRouters);
+
+//middleware error handling
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).send(err.message);
+});
+app.listen(PORT, () => {
+  console.log(`server running on port : ${PORT}`);
+});
