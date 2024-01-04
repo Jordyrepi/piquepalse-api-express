3e8f7a0d46347f0e5c7c003e557144ff82cafeeb
jordy repi <joju19grifith@gmail.com>
2024-01-05 00:53:33 +0700

    fix: fixing get tweets repo

--- src/repositories/tweets/getTweets.ts
+++ src/repositories/tweets/getTweets.ts
@@ -3,6 +3,9 @@ import { prisma } from "../../helper/prisma";
 export const getTweet = async () => {
   try {
     const tweets = await prisma.tweets.findMany({
+      orderBy: {
+        id: "desc",
+      },
       include: {
         User: {
           select: {
